014d564dc0313a7d29058574340fe019f5fc39ef
Ateba Otabela <atebapatrice22@gmail.com>
2025-09-18 21:47:17 -0700

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,8 @@
     <meta content="" name="description">
 
     <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
+   
+    <link href="{{ asset('arkitektur-1.0.0/img/favicon.ico') }}" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -21,9 +22,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <link href="{{asset('arkitektur-1.0.0/lib/animate/animate.min.css')}}" rel="stylesheet">
+    <link href="{{asset('arkitektur-1.0.0/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+    <link href="{{asset('arkitektur-1.0.0/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css')}}" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
     <link href="{{ asset('arkitektur-1.0.0/css/bootstrap.min.css')}}" rel="stylesheet">
@@ -36,7 +37,7 @@
     <!-- Spinner Start -->
     <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
         <div class="spinner-border position-relative text-primary" style="width: 6rem; height: 6rem;" role="status"></div>
-        <img class="position-absolute top-50 start-50 translate-middle" src="img/icons/icon-1.png" alt="Icon">
+        <img class="position-absolute top-50 start-50 translate-middle" src="{{asset('arkitektur-1.0.0/img/icons/icon-1.png')}}" alt="Icon">
     </div>
     <!-- Spinner End -->
 
@@ -70,7 +71,7 @@
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
         <a href="index.html" class="navbar-brand ms-4 ms-lg-0">
-            <h1 class="text-primary m-0"><img class="me-3" src="img/icons/icon-1.png" alt="Icon">Arkitektur</h1>
+            <h1 class="text-primary m-0"><img class="me-3" src="{{asset('arkitektur-1.0.0/img/icons/icon-1.png')}}" alt="Icon">Arkitektur</h1>
         </a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
@@ -102,8 +103,8 @@
     <!-- Carousel Start -->
     <div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="owl-carousel header-carousel position-relative">
-            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-1.jpg'>">
-                <img class="img-fluid" src="img/carousel-1.jpg" alt="">
+            <div class="owl-carousel-item position-relative" data-dot="<img src='{{asset('arkitektur-1.0.0/img/carousel-1.jpg')}}'>">
+                <img class="img-fluid" src="{{asset('arkitektur-1.0.0/img/carousel-1.jpg')}}" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
                         <div class="row justify-content-start">
@@ -116,8 +117,9 @@
                     </div>
                 </div>
             </div>
-            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-2.jpg'>">
-                <img class="img-fluid" src="img/carousel-2.jpg" alt="">
+           <div class="owl-carousel-item position-relative" 
+     data-dot="<img src='{{ asset('arkitektur-1.0.0/img/carousel-2.jpg') }}' alt='carousel dot'>">
+                <img class="img-fluid" src="{{asset('arkitektur-1.0.0/img/carousel-2.jpg')}}" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
                         <div class="row justify-content-start">
@@ -709,17 +711,17 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/counterup/counterup.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="lib/tempusdominus/js/moment.min.js"></script>
-    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
-    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="{{asset('arkitektur-1.0.0/lib/wow/wow.min.js')}}"></script>
+    <script src="{{asset('arkitektur-1.0.0/lib/easing/easing.min.js')}}"></script>
+    <script src="{{asset('arkitektur-1.0.0/lib/waypoints/waypoints.min.js')}}"></script>
+    <script src="{{asset('arkitektur-1.0.0/lib/counterup/counterup.min.js')}}"></script>
+    <script src="{{asset('arkitektur-1.0.0/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('arkitektur-1.0.0/lib/tempusdominus/js/moment.min.js')}}"></script>
+    <script src="{{asset('arkitektur-1.0.0/lib/tempusdominus/js/moment-timezone.min.js')}}"></script>
+    <script src="{{asset('arkitektur-1.0.0/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js')}}"></script>
 
     <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+    <script src="{{asset('arkitektur-1.0.0/js/main.js')}}"></script>
 </body>
 
 </html>
